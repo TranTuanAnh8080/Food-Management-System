e455f4286dfc8cae8f6393cb019935165e93adda
TranTuanAnh8080 <trantuananhhh8088@gmail.com>
2024-11-28 17:05:00 +0700

    tailwind CSS

---
+++ Front_End/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bolder underline">
+        Tailwind CSS !!!
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
